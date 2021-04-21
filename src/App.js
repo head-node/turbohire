f1c922d6232b41a933e1f2e003764ceb414935a5
Kishlay Kumar <kishlay.kumar@publicissapient.com>
2021-04-21 22:55:31 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -2,79 +2,84 @@ import React from "react";
 import "./App.css";
 import About from "./about";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import StatementOfPurpose from "./components/StatementOfPurpose";
 
 export default function App() {
   return (
-    <Router>
+    <>
       <div className="container">
         <div className="sidenav">
-          <div>
-            <Link className="link" to="/about">
-              <div className="div-card">
-                <div className="box"></div>
-                <div className="child-textbox">
-                  <h3>DOCUMENT</h3>
-                  <div className="text">
-                    this is the first div for the container
-                  </div>
-                </div>
-              </div>
-            </Link>
+          <div className="link-box new-form">
+            <div className="frame"></div>
+            <div className="frame-name">
+              <p className="frame-title">New Form</p>
+              <p className="frame-detail">
+                Start creating a new form with the wide options of fields
+                available
+              </p>
+            </div>
           </div>
-          <div>
-            <Link className="link" to="/about">
-              <div className="div-card">
-                <div className="box"></div>
-                <div className="child-textbox">
-                  <h3>DOCUMENT</h3>
-                  <div className="text">
-                    this is the first div for the container
-                  </div>
-                </div>
-              </div>
-            </Link>
+
+          <p className="template">Explore the following Templates:</p>
+          <div className="link-box">
+            <div className="frame"></div>
+            <div className="frame-name">
+              <p className="frame-title">Details Collection</p>
+              <p className="frame-detail">
+                Collect information from Candidates on their education, work
+                experience, contact no,etc
+              </p>
+            </div>
           </div>
-          <div>
-            <Link className="link" to="/about">
-              <div className="div-card">
-                <div className="box"></div>
-                <div className="child-textbox">
-                  <h3>DOCUMENT</h3>
-                  <div className="text">
-                    this is the first div for the container
-                  </div>
-                </div>
-              </div>
-            </Link>
+          <div className="link-box">
+            <div className="frame"></div>
+            <div className="frame-name">
+              <p className="frame-title">Document Collection</p>
+              <p className="frame-detail">
+                Save time and efforts: Explore this template to find a set of
+                questions required for document collection
+              </p>
+            </div>
           </div>
-          <div>
-            <Link className="link" to="/about">
-              <div className="div-card">
-                <div className="box"></div>
-                <div className="child-textbox">
-                  <h3>DOCUMENT</h3>
-                  <div className="text">
-                    this is the first div for the container
-                  </div>
-                </div>
-              </div>
-            </Link>
+          <div className="link-box">
+            <div className="frame"></div>
+            <div className="frame-name">
+              <p className="frame-title">Statement of Purpose</p>
+              <p className="frame-detail">
+                Start creating a new form with the wide options of fields
+                available
+              </p>
+            </div>
           </div>
-          <div>
-            <Link className="link" to="/about">
-              <div className="div-card">
-                <div className="box"></div>
-                <div className="child-textbox">
-                  <h3>DOCUMENT</h3>
-                  <div className="text">
-                    this is the first div for the container
-                  </div>
-                </div>
-              </div>
-            </Link>
+          <div className="link-box">
+            <div className="frame"></div>
+            <div className="frame-name">
+              <p className="frame-title">Interview Availability</p>
+              <p className="frame-detail">
+                Start creating a new form with the wide options of fields
+                available
+              </p>
+            </div>
           </div>
         </div>
-
+        <div className="wrapper-content">
+          <p>Preview</p>
+          <p>You will be able to customize the fields in the later stage</p>
+          <div className="content">
+          <div className="title-content">
+            <div className="title">
+            <p >Name of the Enquiry Form</p>
+            <p>One line description of the form</p>
+            <p>Provide the following information to process your application</p>
+            </div>
+          </div>
+          <div className="main-content">
+            <StatementOfPurpose/>
+          </div>
+          </div>
+        </div>
+      </div>
+      <Router>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
@@ -84,7 +89,7 @@ export default function App() {
           <Route path="/users">{/* <Users >About</Link> */}</Route>
           <Route path="/">{/* <Home >About</Link> */}</Route>
         </Switch>
-      </div>
-    </Router>
+      </Router>
+    </>
   );
 }
